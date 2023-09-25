085e08085e370c2cfbfa5e4e641f47b13d6f809a
Paul De Ulloa <pauld143@gmail.com>
2023-09-24 21:46:26 -0700

    complete

---
+++ Test.java
@@ -0,0 +1,7 @@
+public class Test{
+    public static void main(String[] args){
+        System.out.println("My name is Paul De Ulloa");
+        System.out.println("I am 100 years old");
+        System.out.println("I live in California");
+    }
+}
